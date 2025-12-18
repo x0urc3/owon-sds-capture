8f01896229c1b924e38c2599c6f4fba571fd5749
Khairulmizam Samsudin <xource@gmail.com>
2025-12-18 19:31:17 +0800

    feat: Added cli and OWON SDS USB protocol
    * Ported USB protocol from owoncontrol
    * Added cli arguments to control oscilloscope
    * Added verbosity argument
    * Update README

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# owon-sds-grok
-Owon SDS Oscilloscope software
+# OWON PDS Series Oscilloscope Controller
+
+This project provides a Python utility for controlling OWON PDS series digital oscilloscopes via USB.
+
+## Installation
+
+It is recommended to install the package in a virtual environment.
+
+```bash
+# Install the package in editable mode from the project root
+pip install -e .
+```
+
+### Linux USB Permissions
+
+On Linux, you may need to grant your user permission to access the oscilloscope's USB device. Running with `sudo` is a quick workaround, but the recommended approach is to add a `udev` rule.
+
+1.  Create a new file at `/etc/udev/rules.d/99-owon-pds.rules`.
+
+2.  Add the following line to the file, which matches the OWON PDS series oscilloscopes:
+    ```
+    SUBSYSTEM=="usb", ATTR{idVendor}=="5345", ATTR{idProduct}=="1234", MODE="0666"
+    ```
+
+3.  Save the file and exit the editor.
+
+4.  Reload the `udev` rules for the changes to take effect.
+    ```bash
+    sudo udevadm control --reload-rules
+    sudo udevadm trigger
+    ```
+
+5.  You may need to unplug and reconnect the oscilloscope for the new rule to be applied. After this, you should be able to run `owon-sds-capture` without `sudo`.
+
+## Usage
+
+The application provides a command-line interface for interacting with the oscilloscope.
+
+**Note:** You may need to run the commands with `sudo` or set up appropriate `udev` rules to allow access to the USB device.
+
+### Examples
+
+```bash
+# View all available commands and their arguments
+owon-sds-capture --help
+
+# Perform an auto-set
+owon-sds-capture --autoset
+
+# Revert to factory settings
+owon-sds-capture --factory
+
+# Force a trigger event
+owon-sds-capture --trigger force
+
+# Set Channel 1 coupling to AC
+owon-sds-capture --coupling 1 AC
+
+# Set the timebase to 1ms
+owon-sds-capture --timebase T_1MS
+
+# Set the vertical voltage scale for Channel 2 to 500mV
+owon-sds-capture --volt-scale 2 V_500MV
+
+# Set acquisition mode to Peak Detect
+owon-sds-capture --acq-mode PEAK_DETECT
+
+# Configure an edge trigger for Channel 1
+owon-sds-capture --trigger-type 1 edge --trigger-mode SINGLE --trigger-slope FALLING
+```
+
+## Acknowledgements
+
+The OWON SDS USB protocol command definitions were adapted from the [owoncontrol](https://github.com/7oxicshadow/owoncontrol) project by 7oxicshadow. This Python port would not have been possible without this foundational work.
+
---
+++ src/usb_interface.py
@@ -0,0 +1,111 @@
+"""
+owon-sds-grok
+Copyright (C) 2025 Khairulmizam <xource@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import usb.core
+import usb.util
+import constants
+import logging
+
+class USBInterface:
+    """
+    A class to manage USB communication with the Owon oscilloscope.
+    """
+
+    def __init__(self, vendor_id: int = constants.USB_VENDOR_ID, product_id: int = constants.USB_PRODUCT_ID):
+        """
+        Initializes the USB interface.
+
+        Args:
+            vendor_id: The USB vendor ID of the device.
+            product_id: The USB product ID of the device.
+        """
+        self.vendor_id = vendor_id
+        self.product_id = product_id
+        self.device = None
+        self.kernel_driver_active = False
+
+    def connect(self) -> None:
+        """
+        Finds the USB device, detaches the kernel driver if necessary,
+        and claims the interface.
+        """
+        logging.info(f"Searching for device with VID={hex(self.vendor_id)} PID={hex(self.product_id)}...")
+        self.device = usb.core.find(idVendor=self.vendor_id, idProduct=self.product_id)
+
+        if self.device is None:
+            raise ConnectionError("Owon oscilloscope not found. Check connection and permissions.")
+
+        logging.info("Device found.")
+
+        # Detach kernel driver if active
+        self.kernel_driver_active = self.device.is_kernel_driver_active(0)
+        if self.kernel_driver_active:
+            logging.info("Kernel driver active. Detaching...")
+            self.device.detach_kernel_driver(0)
+            logging.info("Driver detached.")
+
+        # Set the active configuration. With no arguments, the first configuration will be the active one.
+        cfg = self.device.get_active_configuration()
+        if cfg is None:
+            self.device.set_configuration()
+
+        # Claim the interface
+        usb.util.claim_interface(self.device, 0)
+        logging.info("Interface claimed.")
+
+    def disconnect(self) -> None:
+        """
+        Releases the USB interface and disposes of the device object.
+        """
+        if self.device:
+            logging.info("Releasing USB interface...")
+            usb.util.release_interface(self.device, 0)
+            # This is particularly important on Linux where the kernel driver needs to be reattached
+            if self.kernel_driver_active:
+                self.device.attach_kernel_driver(0)
+                logging.info("Interface released and kernel driver reattached.")
+            else:
+                logging.info("Interface released.")
+            self.device = None
+            self.kernel_driver_active = False
+
+
+    def write(self, data: bytes) -> None:
+        """
+        Sends data to the device's bulk write endpoint.
+
+        Args:
+            data: The bytes to send to the oscilloscope.
+        """
+        if self.device is None:
+            raise ConnectionError("Device not connected. Cannot write data.")
+        
+        try:
+            self.device.write(constants.BULK_WRITE_ENDPOINT, data)
+        except usb.core.USBError as e:
+            logging.error(f"Error writing to device: {e}")
+            # Consider re-raising or handling more gracefully
+            raise
+
+    def __enter__(self):
+        """Context manager entry point."""
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Context manager exit point."""
+        self.disconnect()
